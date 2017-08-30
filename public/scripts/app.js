b3667cbf0c2cf4e0400d3e20eab8e977fe3f3200
John Erickson <ericksonjohn08@hotmail.com>
2017-08-29 21:59:15 -0400

    Add ctrlAddItem to global app controller. Add keypress addEventListener.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,29 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-	var x = 23;
-	
-	var add = function(a) {
-		return x + a;
-	}
-	
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-	}
+	// Some code
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
-	
+	// Some code
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-	var z = budgetCtrl.publicTest(5);
+	var ctrlAddItem = function() {
+		// 1. Get the field input data
+		// 2. Add the item to the budget
+		// 3. Add the item to the uI
+		// 4. Calculate the budget
+		// 5. Display the budget on the UI
+		console.log('Hello Kratos!');
+	};
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 	
-	return {
-		anotherPublic: function() {
-			console.log(z);
-		}
-	}
+	document.addEventListener('keypress', function(event) {
+		if(event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}	
+	});
+
 })(budgetController, UIController);
