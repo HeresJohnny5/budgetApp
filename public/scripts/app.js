09328d548fb3354fa3d3a4f58f8dfd107b34556e
John Erickson <ericksonjohn08@hotmail.com>
2017-09-02 12:40:11 -0400

    Add incomeContainer and expensesContainer to private variable domStrings in module UIController. Add conditional statement in public function getInput. Conditional will add id, description, and value to DOM based on type selected.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -21,21 +21,21 @@ var budgetController = (function() {
 			exp: 0,
 			inc: 0
 		}
-	}
+	};
 	
 	return {
 		addItem: function(type, des, val) {
 			var newItem, ID;
 			
 			// CREATE NEW ID
-			if(data.allItems[type].length > 0) {
+			if (data.allItems[type].length > 0) {
 				ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
 			} else {
 				ID = 0;
 			}
 			
 			// CREATE NEW ITEM BASED ON 'inc' or 'exp'
-			if(type === 'exp') {
+			if (type === 'exp') {
 				newItem = new Expense(ID, des, val);
 			} else if (type === 'inc') {
 				newItem = new Income(ID, des, val);
@@ -60,7 +60,9 @@ var UIController = (function() {
 		inputType: '.add__type',
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
-		inputButton: '.add__btn'
+		inputButton: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: '.expenses__list'
 	};
 	
 	return {
@@ -71,6 +73,31 @@ var UIController = (function() {
 				value: document.querySelector(domStrings.inputValue).value
 			};
 			// returning a object is a good way to return multiple variables
+		},
+		addListItem: function(obj, type) {
+			var html, newHtml, element;
+			
+			// create HTML string with placeholder text
+			if (type === 'inc') {
+				element = domStrings.incomeContainer;
+				
+				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			} else if (type === 'exp') {
+				element = domStrings.expensesContainer;
+				
+				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+			
+			// replace the placeholder text with actual data
+			newHtml = html.replace('%id', obj.id);
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
+			
+			// insert the HTML into the DOM
+			/*
+				insertAdjacentHTML with 'beforeend' will insert all HTML as the last child of the element container
+			*/
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 		},
 		getDomStrings: function() {
 			return domStrings;
@@ -86,7 +113,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		document.querySelector(dom.inputButton).addEventListener('click', ctrlAddItem);
 	
 		document.addEventListener('keypress', function(event) {
-			if(event.keyCode === 13 || event.which === 13) {
+			if (event.keyCode === 13 || event.which === 13) {
 				event.preventDefault();
 				ctrlAddItem();
 			}	
@@ -98,12 +125,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 		
 		// 1. Get the field input data
 		input = UICtrl.getInput();
-		console.log(input);
 		
 		// 2. Add the item to the budget
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 		
-		// 3. Add the item to the uI
+		// 3. Add the item to the UI
+		UIController.addListItem(newItem, input.type);
+		
 		// 4. Calculate the budget
 		// 5. Display the budget on the UI
 	};
