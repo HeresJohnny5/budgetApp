a59f734634824780b5fedb1595861e1d315a93e4
John Erickson <ericksonjohn08@hotmail.com>
2017-09-01 21:53:47 -0400

    Create public method addItem to module budgetController. Create conditional statement to create new ID and add new item based on type.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -22,6 +22,36 @@ var budgetController = (function() {
 			inc: 0
 		}
 	}
+	
+	return {
+		addItem: function(type, des, val) {
+			var newItem, ID;
+			
+			// CREATE NEW ID
+			if(data.allItems[type].length > 0) {
+				ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+			} else {
+				ID = 0;
+			}
+			
+			// CREATE NEW ITEM BASED ON 'inc' or 'exp'
+			if(type === 'exp') {
+				newItem = new Expense(ID, des, val);
+			} else if (type === 'inc') {
+				newItem = new Income(ID, des, val);
+			}
+			
+			// PUSH INTO DATA STRUCTURE
+			data.allItems[type].push(newItem); // type will either be exp or inc
+			
+			// RETURN NEW ELEMENT
+			return newItem;
+		},
+		
+		testing: function() {
+			console.log(data);
+		}
+	};
 })();
 
 // UI CONTROLLER
@@ -64,11 +94,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 	};
 		
 	var ctrlAddItem = function() {
+		var input, newItem;
+		
 		// 1. Get the field input data
-		var input = UICtrl.getInput();
+		input = UICtrl.getInput();
 		console.log(input);
 		
 		// 2. Add the item to the budget
+		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+		
 		// 3. Add the item to the uI
 		// 4. Calculate the budget
 		// 5. Display the budget on the UI
