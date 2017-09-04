b6b6d8cc592f8cbca38bced5ba7f0bf66a0e8e58
John Erickson <ericksonjohn08@hotmail.com>
2017-09-04 18:34:15 -0400

    Add deleteItem public method to budgetController module. Invoke budgetCtrl.deleteItem in ctrlDeleteItem private method in controller module.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -61,6 +61,22 @@ var budgetController = (function() {
 			return newItem;
 		},
 		
+		deleteItem: function(type, id) {
+			var ids, index;
+			
+			ids = data.allItems[type].map(function(current) {
+				return current.id;
+			});
+			
+			index = ids.indexOf(id);
+						
+			if (index !== -1) {
+				data.allItems[type].splice(index, 1); 
+				// the first argument is the position number you want to start deleting
+				// the second argument is the number of elements that you want to delete
+			}
+		},
+		
 		calculateBudget: function() {
 			// calculate total income and expenses
 			calculateTotal('exp');
@@ -231,14 +247,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 	var ctrlDeleteItem = function(event) {
 		var itemID, splitID, type, ID;
 		
-		itemId = event.target.parentNode.parentNode.parentNode.parentNode.id;
+		itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
 		
 		if (itemID) {
-			splitID = itemID.splie('-');
+			splitID = itemID.split('-');
 			type = splitID[0];
-			ID = splitID[1];
+			ID = parseInt(splitID[1]);
 			
 			// 1. delete the item from the data structure
+			budgetCtrl.deleteItem(type, ID);
 			
 			// 2. delete the item from the UI
 			
